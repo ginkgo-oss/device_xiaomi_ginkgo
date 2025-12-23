b8b8754a27cf6caa05ee22a3ea51fb8af4b72988
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2025-12-23 15:24:56 +0100

    ginkgo: import product.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/bt_firmware:/bt_firmware
 
 # Properties
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
