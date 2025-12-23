baca779c98e53ccc86b0b95bd1aded9a480d470d
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2025-12-23 15:06:48 +0100

    Revert "ginkgo: Setup Retrofit Partitions"
    This reverts commit 545783066bf552dc194680a8e27fd6e4d805037b.

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -26,8 +26,6 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 # Device identifier
 PRODUCT_NAME := lineage_ginkgo
 PRODUCT_DEVICE := ginkgo
